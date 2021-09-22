fe1be2eab70479d607cacdf3b43df6fb054c5dab
T.SooranKim <t.soorankim@gmail.com>
2021-09-23 04:11:40 +0900

    처리 로직과 복사 기능 추가

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -8,8 +8,6 @@
     '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
     '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
 
-  var input = null;
-
   function validUrl(url) {
     return !!VALID_URL_REGEX.test(url);
   }
@@ -18,6 +16,8 @@
     var status = 'none'; // 'none' | 'correct' | 'wrong'
     var $box = document.querySelector('#input');
     var $input = $box.querySelector('input[name=url]');
+    var $message = $box.querySelector('#message');
+    var messageTimeoutId = null;
     
     function handleInput() {
       var url = $input.value;
@@ -46,7 +46,24 @@
       }
     }
 
+    function showMessage(msg) {
+      if (messageTimeoutId) {
+        clearTimeout(messageTimeoutId);
+      }
+      $message.style.display = 'none';
+      $message.style.display = '';
+      $message.textContent = msg;
+      $message.classList.add('show');
+      messageTimeoutId = setTimeout(function() {
+        messageTimeoutId = null;
+        $message.classList.remove('show');
+      }, 3000);
+    }
+
     $input.addEventListener('input', handleInput);
+    $message.addEventListener('click', function() {
+      $message.classList.remove('show');
+    });
 
     return {
       value: function() {
@@ -61,29 +78,75 @@
           $input.disabled = 'disabled'
         } else {
           $box.classList.remove('process');
-          delete $input.disabled;
+          $input.disabled = '';
         }
       },
-      message: function(msg) {
-
-      }
-    }
+      message: showMessage
+    };
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (input.status() === 'correct') {
-      input.process(true);
-      setTimeout(function() {
-        input.process(false);
-        input.message('끝!');
-      }, 3000);
+  function Result(input) {
+    var $result = document.querySelector('#result');
+    var $url = $result.querySelector('#shorten-url');
+    var $clipboard = $result.querySelector('#clipboard');
+    var copyTimeoutId = null;
+
+    function handleCopy() {
+      if (copyTimeoutId) {
+        clearTimeout(copyTimeoutId);
+      }
+      $clipboard.classList.add('copy');
+      input.message('복사가 완료되었습니다');
+      navigator.clipboard.writeText($url.textContent);
+      copyTimeoutId = setTimeout(function() {
+        $clipboard.classList.remove('copy');
+        copyTimeoutId = null;
+      }, 2000)
     }
+
+    $clipboard.addEventListener('click', handleCopy);
+
+    return {
+      url: function(url) {
+        $result.classList.add('show');
+        $url.textContent = url;
+      },
+      hide: function() {
+        $result.classList.remove('show');
+      }
+    };
+  }
+
+  function Form(input, result) {
+    var processing = false;
+    var $form = document.querySelector('#form');
+
+    function handleSubmit(e) {
+      e.preventDefault();
+      if (processing) {
+        return;
+      }
+
+      if (input.status() === 'correct') {
+        processing = true;
+        input.process(true);
+        result.hide();
+        setTimeout(function() {
+          processing = false;
+          input.process(false);
+          input.message('주소가 성공적으로 줄여졌습니다 복사해서 사용하세요 :)');
+          result.url('https://to2.kr/abc');
+        }, 1000);
+      }
+    }
+
+    $form.addEventListener('submit', handleSubmit);
   }
 
   function handleDocumentLoad() {
-    input = Input();
-    document.querySelector('#form').addEventListener('submit', handleSubmit);
+    var input = Input();
+    var result = Result(input);
+    Form(input, result);
   }
 
   window.addEventListener('load', handleDocumentLoad);
