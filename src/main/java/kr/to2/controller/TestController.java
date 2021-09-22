5f116ea69bd7eec52e46601d5ac4fa5d66037ead
T.SooranKim <t.soorankim@gmail.com>
2021-09-22 15:54:53 +0900

    thymeleaf 추가
    Co-authored-by: jhs512 <jangka512@gmail.com>

--- src/main/java/kr/to2/controller/TestController.java
+++ src/main/java/kr/to2/controller/TestController.java
@@ -1,13 +1,13 @@
 package kr.to2.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@Controller("/")
 public class TestController {
 
   @GetMapping()
   public String test() {
-    return "Hello";
+    return "home";
   }
 }
