726ef9dc2fc9f2cdf9b25cebc6c4b792fb729926
T.SooranKim <t.soorankim@gmail.com>
2021-09-23 23:34:27 +0900

    recaptcha 설정 방어코드 추가

--- src/main/java/kr/to2/config/RecaptchaProperties.java
+++ src/main/java/kr/to2/config/RecaptchaProperties.java
@@ -1,5 +1,7 @@
 package kr.to2.config;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,4 +14,17 @@ public class RecaptchaProperties {
   private String siteKey;
   private String privateKey;
   private boolean active;
+
+  @PostConstruct
+  private void postConstruct() {
+    if (this.active) {
+      if (isEmptyString(this.siteKey) || isEmptyString(this.privateKey)) {
+        throw new RuntimeException("recaptcha.siteKey 또는 recaptcha.privateKey가 필요합니다.");
+      }
+    }
+  }
+
+  private static boolean isEmptyString(String string) {
+    return string == null || string.isBlank();
+  }
 }
