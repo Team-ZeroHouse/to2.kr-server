58c7fadcabc89fdc0e04bbee2a6b4080e855a818
T.SooranKim <t.soorankim@gmail.com>
2021-09-24 17:53:58 +0900

    String util class 생성

--- src/main/java/kr/to2/config/RecaptchaProperties.java
+++ src/main/java/kr/to2/config/RecaptchaProperties.java
@@ -5,6 +5,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import kr.to2.util.Strings;
 import lombok.Data;
 
 @Data
@@ -18,13 +19,9 @@ public class RecaptchaProperties {
   @PostConstruct
   private void postConstruct() {
     if (this.active) {
-      if (isEmptyString(this.siteKey) || isEmptyString(this.privateKey)) {
+      if (Strings.isEmptyString(this.siteKey) || Strings.isEmptyString(this.privateKey)) {
         throw new RuntimeException("recaptcha.siteKey 또는 recaptcha.privateKey가 필요합니다.");
       }
     }
-  }
-
-  private static boolean isEmptyString(String string) {
-    return string == null || string.isBlank();
   }
 }
---
+++ src/main/java/kr/to2/util/Strings.java
@@ -0,0 +1,9 @@
+package kr.to2.util;
+
+public final class Strings {
+
+  public static boolean isEmptyString(String string) {
+    return string == null || string.isBlank();
+  }
+  
+}
