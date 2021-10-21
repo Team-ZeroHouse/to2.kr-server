2fae1309b72fb7e1b4bd9e1b7c074fe5bba57581
T.SooranKim <t.soorankim@gmail.com>
2021-10-22 03:27:14 +0900

    test: fail 일부로 추가

---
+++ src/test/java/kr/to2/FailTest.java
@@ -0,0 +1,14 @@
+package kr.to2;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FailTest {
+
+  @Test
+  public void fail() {
+    assertTrue(false);
+  }
+  
+}
